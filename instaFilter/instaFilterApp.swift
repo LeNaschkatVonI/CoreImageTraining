afa242981414634858e4947fd3c550870a167c80
Maxim Vainikka <rithmique@yahoo.com>
2021-06-29 17:17:22 +0300

    Initial Commit

---
+++ instaFilter/instaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  instaFilterApp.swift
+//  instaFilter
+//
+//  Created by Максим Нуждин on 29.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct instaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
